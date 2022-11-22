cf91148ac2bd057ea0934148c6418a48ae75221e
kehan <kehan@fanmiot.cn>
2022-11-22 14:03:36 +0800

    pref: 完善按钮级别权限控制功能

--- src/plugins/authority.js
+++ src/plugins/authority.js
@@ -2,9 +2,21 @@ import { USER_AUTH } from '@/utils/root/localStorageKeys'
 import ls from '@/utils/lsOperation'
 
 /**
- * 
- *  USER_AUTH :  { action: "usero:edit", describe: "用户编辑", type: "1", status: "1" }
- * 
+ *
+ * @param {string} auth 权限名
+ * 返回授权策略
+ */
+function getAuthType(auth) {
+	const authList = JSON.parse(ls.get(USER_AUTH))
+	let authData = authList.find((item) => item.action === auth)
+	let { type } = authData
+	return type
+}
+
+/**
+ *
+ *  USER_AUTH :  { action: "user:edit", describe: "用户编辑", type: "1", status: "1" }
+ *
  */
 const AuthorityPlugin = {
 	install(Vue) {
@@ -12,21 +24,16 @@ const AuthorityPlugin = {
 			inserted(el, binding, vnode) {
 				const check = binding.value
 				const instance = vnode.context
-                const $auth = instance.$auth
-                let result = $auth(check)
-                if(result === false){
-                    el.parentNode.removeChild(el);
-                }else{
-                    let {type} = result;
-                    switch(type){
-                        case '1':
-                            el.parentNode.removeChild(el);
-                            break;
-                        case '2':
-                            el.classList.add('disabled')
-                            break;
+				const $auth = instance.$auth
+				let result = $auth(check)
+				if (result) {
+					let ctrlType = getAuthType(check)  
+                    if(ctrlType == '1'){
+                        el.parentNode.removeChild(el)
+                    }else if(ctrlType == '2'){
+                        el.classList.add('disabled')
                     }
-                }
+				}
 			},
 		})
 
@@ -37,33 +44,14 @@ const AuthorityPlugin = {
 				 * @param {string} auth 权限名
 				 * @param {number} type 授权策略 type=1 :可见/可访问(授权后可见/可访问); type=2 :可编辑(未授权时禁用)
 				 */
-				$auth(auth, type) {
-                    const authList = ls.get(USER_AUTH)
-                    console.log('authList', authList);
-                    const visibleTypeAuthList = authList.filter(item => item.type == '1')
-                    const disableTypeAuthList = authList.filter(item => item.type == '2')
-                    if(authList.findIndex(item => item.action === auth) !== -1){
-                        let result = {
-                            permission : true,
-                        }
-                        let ctrlType = '';
-                        if(visibleTypeAuthList.findIndex(item => item.action === auth) !== -1){
-                            ctrlType = '1'
-                        }else if(disableTypeAuthList.findIndex(item => item.action === auth) !== -1){
-                            ctrlType = '2'
-                        }
-
-                        type && (ctrlType = type)
-                        
-                        Object.assign(result, {
-                            type : ctrlType
-                        })
-
-                        return result
-                    }else{
-                        return false
-                    }
-                },
+				$auth(auth) {
+					const authList = JSON.parse(ls.get(USER_AUTH))
+					if (authList.findIndex((item) => item.action === auth) !== -1) {
+						return true
+					} else {
+						return false
+					}
+				},
 			},
 		})
 	},
