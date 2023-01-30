23a97ec174c0048689fe8a33b6f6c0e282968fa6
kehan <kehan@fanmiot.cn>
2023-01-30 10:33:56 +0800

    build: 升级less依赖版本

--- vue.config.js
+++ vue.config.js
@@ -217,9 +217,12 @@ module.exports = function () {
 			loaderOptions: {
 				// 给 less-loader 传递 Less.js 相关选项
 				less: {
-					// `globalVars` 定义全局对象，可加入全局变量
-					globalVars: {
-						// primary: '#333'
+					lessOptions: {
+						// `globalVars` 定义全局对象，可加入全局变量
+						globalVars: {
+							// primary: '#333'
+						},
+						javascriptEnabled : false,
 					},
 				},
 			},
